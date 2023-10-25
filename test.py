37fb5db70c81264b53c3340305b1ce44a166bc6c
Salahor <salahor20@gmail.com>
2023-10-25 09:09:40 +0200

    test 50

--- test.py
+++ test.py
@@ -32,6 +32,15 @@ class TestNombreRomain(unittest.TestCase):
         romain = ArabeToRomain.NbrRomain(nombre)
         # test 3
         self.assertEqual(romain, 'IV')
+        #etant donnee nombre 50
+        nombre =50
+        #etant donnee nombre romain L
+        romain=nr(nombre)
+        #test nombre 50
+        self.assertEqual(romain,'L')
+
+
+
 
 
 
