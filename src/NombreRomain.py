2f3b0377e1dd084c72829378877d42806e62d426
Salahor <salahor20@gmail.com>
2023-10-25 11:57:48 +0200

    AJouter Methode pour convertir le nombre arabe

--- src/NombreRomain.py
+++ src/NombreRomain.py
@@ -13,31 +13,7 @@ class ArabeToRomain():
         else:
             return 'IV'
 
-    def NombreRomain(number):
-        nombre_romain = {
-            1: 'I', 4: 'IV', 5: 'V', 9: 'IX', 10: 'X', 40: 'XL',
-            50: 'L', 90: 'XC', 100: 'C', 400: 'CD', 500: 'D', 900: 'CM', 1000: 'M'
-        }
-        resultat = ''
-        for chiffre, symbole in sorted(nombre_romain.items(), reverse=True):
-            while number >= chiffre:
-                resultat += symbole
-                number -= chiffre
-        return resultat
-
-    def NbrRomain(num):
-        liste1 = ['I', 'IV', 'V']
-        liste2 = [1, 4, 5]
-        resultat = ''
-        for i in range(len(liste2) - 1, -1, -1):
-            while num >= liste2[i]:
-                resultat += liste1[i]
-                num -= liste2[i]
-        return resultat
 
 
 
 
-
-
-print(ArabeToRomain.NbrRomain(49))
